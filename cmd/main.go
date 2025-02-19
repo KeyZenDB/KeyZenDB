ecd3ce47af8fdc3568c1c0191cc2236622dbfcfd
Mahdi Mohammadi <mahdicsharp0098@gmail.com>
2025-02-19 12:12:13 +0330

    feat: add config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/KeyZenDB/KeyZenDB/config"
+)
+
+func main() {
+	configPath := flag.String("config", "config.toml", "config file")
+	flag.Parse()
+	_ = config.LoadConfig(*configPath)
 }
---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	App struct {
+		Port string
+	}
+}
+
+var once sync.Once
+
+func LoadConfig(address string) (config *Config) {
+	once.Do(func() {
+		viper.SetConfigFile(address)
+		if err := viper.ReadInConfig(); err != nil {
+			panic(err)
+		}
+		if err := viper.Unmarshal(&config); err != nil {
+			panic("ERROR load config file!")
+		}
+		log.Println("================ Loaded Configuration ================")
+	})
+	return
+}
